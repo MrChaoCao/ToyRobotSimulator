21f27e9d8b718a922c7d150db5ef06eba9556753
chao <mailbox.chao.cao@gmail.com>
2018-09-10 11:42:30 -0700

    Add 0.5s time delay for all commands executed by the toy robot

--- src/Simulator.js
+++ src/Simulator.js
@@ -9,10 +9,16 @@ export default class Simulator {
   }
 
   executeCommands(){
-    while (this.commands.length > 0) {
-      let nextLine = this.commands.shift();
-      let nextCommand = this.inputToCommand(nextLine)
-    }
+    // while (this.commands.length > 0) {
+    //   let nextLine = this.commands.shift();
+    //   let nextCommand = this.inputToCommand(nextLine)
+    // }
+    setInterval( () => {
+      if (this.commands.length > 0) {
+        let nextLine = this.commands.shift();
+        let nextCommand = this.inputToCommand(nextLine)
+      }
+    }, 500);
   }
 
   inputToCommand(userInput){
