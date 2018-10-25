f704a1f8759c23729af4d4b3bf7b53223da4ddbe
chao <mailbox.chao.cao@gmail.com>
2018-10-24 20:03:38 -0700

    Add jest testing for simulator class

--- src/Simulator.test.js
+++ src/Simulator.test.js
@@ -1,50 +1,64 @@
-import Simulator from './Simulator'
 import Robot from './Robot'
+import Simulator from './Simulator'
 
 const testRobot = new Robot(0,0,0);
-const testSim = new Simulator();
+const testSim = new Simulator('place 0,0,north\n move');
 
-describe('', () => {
-  test('reportCommand method calls Robot report function and receives robot\'s location data', () => {
+// const jestFn = jest.fn()
 
+describe('simpleCommandCenter properly routes simple commands', () => {
+  const rotateSpy = jest.spyOn(Robot.prototype, 'rotate')
+  test("reportCommand method calls Robot report function and receives robot's location data", () => {
+    // const reportSpy = jest.spyOn(Robot.prototype, 'report')
+    // const Robot.report = jest.fn();
+    // console.log(testRobot.report);
+    // testSim.reportCommand();
+    // expect(testRobot.report).toHaveBeenCalled();
   });
   test('left command calls rotateCommand function with -90', () => {
-
+    testSim.simpleCommandCenter('left');
+    expect(rotateSpy).toHaveBeenCalledWith(-90);
   });
   test('right command calls rotateCommand function with 90', () => {
-
+    testSim.simpleCommandCenter('right');
+    expect(rotateSpy).toHaveBeenCalledWith(90);
   });
-  test('move command calls Robot rotateCommand function with "right" ', () => {
-
-  });
-});
-
-
-describe('', () => {
-  test('placeCommandCenter calls Robot.updatePosition with user input arguments', () => {
-
+  test('move command calls Robot interpretMove function', () => {
+    const moveSpy = jest.spyOn(Robot.prototype, 'interpretMove')
+    testSim.simpleCommandCenter('move');
+    expect(rotateSpy).toHaveBeenCalled();
   });
 });
 
-describe('', () => {
-  const listOfCommands = [
-    ''
-  ]
-  test('Commmand router ignores all commands until a valid place command is made', () => {
 
-  });
-  test('Commmand router sends simple commands to the simple command router', () => {
+// describe('placeCommandCenter functions properly', () => {
+//   test('placeCommandCenter calls Robot.updatePosition with user input arguments', () => {
+//     const placeTestSim = new Simulator('place 2,2,west')
+//     const placeCommandSpy = jest.spyOn(Simulator.prototype, 'placeCommandCenter')
+//     // placeTestSim.commandRouter('place 2,2,west')
+//     placeTestSim.executeCommands();
+//     expect(placeCommandSpy).toHaveBeenCalledWith(2,2,'west')
+//   });
+// });
 
+describe('commandRouter functions properly', () => {
+  const simpleTestSim = new Simulator('move\nleft\nrigfht\nplace 0,0,west')
+  describe('Commmand router ignores all commands until a valid place command is made', () => {
+    const simpleCommandSpy = jest.spyOn(Simulator.prototype, 'simpleCommandCenter')
+    simpleTestSim.executeCommands()
+    expect(simpleCommandSpy).toHaveBeenCalledTimes(0)
   });
+  // describe('Commmand router sends simple commands to the simple command router', () => {
+  //   const simpleCommandSpy = jest.spyOn(Simulator.prototype, 'simpleCommandCenter')
+  //   // const commandRouterTestSim = new Simulator('place 0,0,west\nmove\nmove\nleft\nright\nplace 1,1,east')
+  //   // commandRouterTestSim.executeCommands();
+  //   commandRouter('left')
+  //   expect(simpleCommandSpy).toHaveBeenCalledTimes(4)
+  // });
   test('Commmand router sends place commands to the place command router', () => {
 
   });
   test('Command router ignores all strings that do not match', () => {
-
-  });
-});
-describe.skip('', () => {
-  test.skip('', () => {
 
   });
 });
